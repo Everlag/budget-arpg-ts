3f0e4a8c154469884d46e120807774cb7266d9ef
Everlag <kyleveb@gmail.com>
2016-12-30 00:04:43 -0700

    checkpoint vue simple state display

--- src/visualize.ts
+++ src/visualize.ts
@@ -4,18 +4,51 @@ import {
     IRecord,
     //     IMovementRecord, IDamageRecord, IDeathRecord
 } from './Records';
-import { StateSerial, PackSerial } from './Serial';
+import { StateSerial, PackSerial, CharacterStateSerial } from './Serial';
 // import * as d3 from 'd3';
 import * as Vue from 'vue';
 import Component from 'vue-class-component';
 
-import root from './components/root';
+// import root from './components/root';
+
+@Component({
+    props: {
+        character: Object,
+    },
+    template: `
+    <div>
+        <b>{{character.EntityCode}} at {{clean(character.Position)}}</b>
+        <div>{{clean(character.health)}}/{{clean(character.maxHealth)}}</div>
+        <div>{{clean(character.mana)}}/{{clean(character.maxMana)}}</div>
+    </div>`,
+})
+class Character extends Vue {
+    private readonly character: CharacterStateSerial;
+
+    public mounted() {
+        if (!this.character) return;
+        console.log(`holy shit I'm mounted with entity=${this.character.EntityCode}!`);
+    }
+
+    // Cleanup a number for display
+    public clean(value: number): number {
+        return Math.floor(value);
+    }
+}
 
 @Component({
     props: {
         pack: Object,
     },
-    template: `<div>okay, so we're here! {{pack.isDead}}</div>`,
+    template: `
+    <div>
+        <character
+            v-for="char in characters" 
+            v-bind:character="char"></character>
+    </div>`,
+    components: {
+        Character,
+    }
 })
 class Pack extends Vue {
     private readonly pack: PackSerial;
@@ -23,6 +56,38 @@ class Pack extends Vue {
     public mounted() {
         if (!this.pack) return;
         console.log(`holy shit I'm mounted with isDead=${this.pack.isDead}!`);
+    }
+
+    public get characters(): Array<CharacterStateSerial> {
+        return this.pack.states;
+    }
+}
+
+@Component({
+    props: {
+        state: Object,
+    },
+    template: `
+    <div>
+        <div>Tick-Time={{state.when}}</div>
+        <template v-for="(pack, index) in packs">
+            <hr v-if="index">
+            <pack :pack="pack"></character>
+        </template>
+    </div>`,
+    components: {
+        Pack,
+    }
+})
+class State extends Vue {
+    private readonly state: StateSerial;
+
+    public mounted() {
+        if (!this.state) return;
+    }
+
+    public get packs(): Array<PackSerial> {
+        return this.state.packs
     }
 }
 
@@ -33,17 +98,18 @@ export function renderVue() {
         template: `
             <div>
                 <div>apples</div>
-                <pack :pack='pack'></pack>
-                <root propMessage='apples'></root>
+                <state :state="state"></pack>
             </div>
         `,
         components: {
-            root, Pack,
+            State,
         },
         data: () => {
             return {
-                pack: {
-                    isDead: false,
+                state: {
+                    when: 0,
+                    packs: [],
+                    events: [],
                 },
                 propMessage: 'holy shit!',
             };
@@ -55,7 +121,7 @@ export function renderVue() {
     let snapshots: Array<string> = (<any>window).snapshots;
     let renderState: StateSerial = JSON.parse(snapshots[0]);
 
-    (<any>mount.$data).pack = renderState.packs[0];
+    (<any>mount.$data).state = renderState;
 }
 
 export function visualize(seedState: Object, events: Array<IRecord>) {
