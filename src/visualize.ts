351a34bf84693f1872493ab206134f3922e05bd9
Everlag <kyleveb@gmail.com>
2016-12-29 20:27:53 -0700

    checkpoint vue data flow thoughts

--- src/visualize.ts
+++ src/visualize.ts
@@ -4,22 +4,58 @@ import {
     IRecord,
     //     IMovementRecord, IDamageRecord, IDeathRecord
 } from './Records';
+import { StateSerial, PackSerial } from './Serial';
 // import * as d3 from 'd3';
 import * as Vue from 'vue';
+import Component from 'vue-class-component';
 
 import root from './components/root';
+
+@Component({
+    props: {
+        pack: Object,
+    },
+    template: `<div>okay, so we're here! {{pack.isDead}}</div>`,
+})
+class Pack extends Vue {
+    private readonly pack: PackSerial;
+
+    public mounted() {
+        if (!this.pack) return;
+        console.log(`holy shit I'm mounted with isDead=${this.pack.isDead}!`);
+    }
+}
 
 export function renderVue() {
     // mount
     let mount = new Vue({
         el: '#el',
-        template: `<root propMessage='apples'></root>`,
+        template: `
+            <div>
+                <div>apples</div>
+                <pack :pack='pack'></pack>
+                <root propMessage='apples'></root>
+            </div>
+        `,
         components: {
-            root,
+            root, Pack,
+        },
+        data: () => {
+            return {
+                pack: {
+                    isDead: false,
+                },
+                propMessage: 'holy shit!',
+            };
         },
     });
     if (!mount) throw Error('vue not mounted');
     console.log(mount);
+
+    let snapshots: Array<string> = (<any>window).snapshots;
+    let renderState: StateSerial = JSON.parse(snapshots[0]);
+
+    (<any>mount.$data).pack = renderState.packs[0];
 }
 
 export function visualize(seedState: Object, events: Array<IRecord>) {
