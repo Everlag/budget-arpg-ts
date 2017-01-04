9c85dcb1afb77b72b8fa01ad1b71476a2f434f47
Everlag <kyleveb@gmail.com>
2017-01-04 00:16:11 -0700

    d3 moving dots on a line
    This would allow us to expose an interface where, at least, movement
    events are fed in and the displayed state be manipulated. Holy crap!

--- src/fancyvis.ts
+++ src/fancyvis.ts
@@ -2,8 +2,14 @@ import { select, Selection } from 'd3-selection';
 // Needed as a side efect to inject transition
 // into the selection api.
 import 'd3-transition';
+import { transition } from 'd3-transition';
+import { interval } from 'd3-timer';
 import { line, curveCardinalClosed } from 'd3-shape';
+import { axisBottom } from 'd3-axis';
+import { scaleLinear, ScaleLinear } from 'd3-scale';
 import { interpolateString } from 'd3-interpolate';
+
+import { intfromInterval } from './random';
 
 interface ILinePoint {
     x: number;
@@ -51,9 +57,123 @@ function curve(root: Selection<any, any, any, any>,
 
 }
 
-interface ICircleSpec {
-    radius: number;
+interface IPoint {
+    pos: number;
     id: string;
+}
+
+interface IMove {
+    duration: number;
+    newPos: number;
+    id: string;
+}
+
+const graphRootID = 'graphRootID';
+
+function graphMargins(width: number, height: number): [number, number] {
+    // Handle our margins
+    let margin = {
+        top: 20, right: 20, bottom: 20, left: 20,
+    };
+
+    width = width - (margin.left + margin.right);
+    height = height - (margin.top + margin.bottom);
+    return [width, height];
+}
+
+function prepGraph(root: Selection<any, any, any, any>,
+    width: number, height: number): ScaleLinear<number, number> {
+
+    // Consider our margins
+    [width, height] = graphMargins(width, height);
+
+    // Create our range and set it's domain
+    let xDomain = [-100, 100];
+    let xRange = [0, width];
+    let x = scaleLinear().range(xRange);
+    x.domain(xDomain);
+
+    let axisHeight = 50;
+
+    let group = root.append('g')
+        .attr('id', graphRootID)
+        .attr('width', width)
+        .attr('height', height);
+
+    // Add the axis
+    let axis = group.append('g')
+        // Ensure its in the middle
+        .attr('transform', `translate(0, ${height / 2})`)
+        .attr('height', axisHeight)
+        // We want only the center marker
+        .call(axisBottom(x).ticks(1));
+
+    return x;
+}
+
+function graph(root: Selection<any, any, any, any>,
+    width: number, height: number,
+    xScale: ScaleLinear<number, number>,
+    points: Array<IPoint>) {
+
+    // Consider our margins
+    [width, height] = graphMargins(width, height);
+
+    // Grab the root of our graph
+    let markers = select(`#${graphRootID}`).selectAll('circle');
+
+    let inTransition = transition('markersIn').duration(2000);
+
+    // UPDATE
+    markers.attr('class', 'update')
+        .transition().duration(() => 20);
+
+    // ENTER
+    markers.data(points, (p: IPoint) => p.id)
+        .enter()
+        .append('circle')
+        .attr('class', 'enter')
+        .attr('r', 10)
+        // Convert simulation position to display coord
+        .attr('cx', d => xScale(d.pos))
+        // Start at +-height and fade in
+        .attr('cy', (d, i) => Math.pow(-1, i) * height)
+        .style('opacity', 0)
+        // Transition the markers into position
+        .transition(inTransition)
+        .style('opacity', 1)
+        .attr('cy', height / 2);
+
+    // EXIT
+    markers.exit().remove();
+
+    // let valueLine = line<IPoint>()
+    //     .x(d => x(d.pos))
+    //     .y(d => 0);
+}
+
+function move(root: Selection<any, any, any, any>,
+    width: number, height: number,
+    xScale: ScaleLinear<number, number>,
+    move: Array<IMove>) {
+
+    // Consider our margins
+    [width, height] = graphMargins(width, height);
+
+    // Generate a lookup table of ids that moved
+    let movedIDs = new Map<string, boolean>();
+    move.forEach(m => movedIDs.set(m.id, true));
+
+    // Grab the markers that moved
+    let moved = select(`#${graphRootID}`).selectAll('circle')
+        .data(move, (p: IMove) => p.id)
+        // Filter only for those that moved
+        .filter((d: IMove) => movedIDs.has(d.id));
+
+    moved.transition()
+        .duration(d => d.duration)
+        .attr('cx', d => xScale(d.newPos))
+
 }
 
 export function visualize() {
@@ -96,12 +216,44 @@ export function visualize() {
     // Create a group for our content
     let groot = svgroot.append('g');
 
-    let curveData: Array<[number, number]> = [
-        [480, 200],
-        [580, 400],
-        [680, 100],
-        [780, 300],
-    ];
-    curve(groot, curveData);
+    let xScale = prepGraph(groot, width, height);
 
+    let i = 0;
+    let points: Array<IPoint> = [
+        {
+            id: `${++i}`,
+            pos: 0,
+        },
+        {
+            id: `${++i}`,
+            pos: 30,
+        },
+        {
+            id: `${++i}`,
+            pos: -70,
+        },
+        {
+            id: `${++i}`,
+            pos: -20,
+        },
+        {
+            id: `${++i}`,
+            pos: 80,
+        },
+    ]
+    graph(groot, width, height, xScale, points);
+
+    let moves: Array<IMove> = [
+        {
+            duration: 4000,
+            newPos: -100,
+            id: `${i}`,
+        },
+    ]
+
+    interval(() => {
+        moves[0].newPos = intfromInterval(-100, 100);
+        console.log('moving to', moves[0].newPos);
+        move(groot, width, height, xScale, moves);
+    }, 2000);
 }
